c6ce178fa892eb64de1a5d60786cc1f370eb411e
aurelianvoinea6 <aurash1991@hotmail.com>
2020-11-09 09:57:11 +0000

    fixed and added a background

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
-var pronoun = ['the','our'];
-var adj = ['great', 'big' ];
-var noun = ['jogger','racoon'];
+let pronoun = ['the','our'];
+let adj = ['great', 'big' ];
+let noun = ['jogger','racoon'];
+let final = [".es", ".it", ".ro", ".de"];
 
-let domain = [];
+let domainGenerator = [];
 
-for (p in pronoun)
-    for (a in adj)
-        for (n in noun)
-            domain.push(pronoun[p]+adj[a]+noun[n]+'.com');
+for (let a = 0; a < pronoun.length; a++)
+    for (let b = 0; b < adj.length; b++) 
+        for (let c = 0; c < noun.length; c++)
+            for (let d = 0; d < final.length; d++)
+                domainGenerator.push(pronoun[a]+adj[b]+noun[c]+final[d]);
 
-let div = document.querySelector('.domain_generator');
+    
+console.log(domainGenerator);
 
-for (i in domain) {
+let divDomain = document.querySelector(".domain_generator");
+
+for (let index = 0; index < domainGenerator.length; index++) {
     let node = document.createElement('p');
-    let text = document.createTextNode(domain[i]);
+    let text = document.createTextNode(domainGenerator[index]);
     node.appendChild(text);
-    div.appendChild(node);
+    divDomain.appendChild(node);
 }
